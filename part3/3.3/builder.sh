bb0b5e0b8e79253ace84bc550c3a515609f7b049
Mikko Nikkilä <mikko.nikkila@iki.fi>
2023-06-16 17:11:46 +0300

    Exercise 3.3

---
+++ part3/3.3/builder.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+github=$1
+dockerhub=$2
+
+echo -n "Cloning from GitHub... "
+git clone $github
+echo "OK."
+echo -n "Building Docker image..."
+docker build . -t $dockerhub
+echo "OK."
+echo -n "Pushing to DockerHub..."
+docker image push $dockerhub
+echo "OK."
